3b91775da727aba780bae6bcd07fa53f9d81bd71
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-04 20:23:47 +0200

    Add CategoryExpense model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
-  has_many :category_expenses, dependent: :destroy
-  has_many :expenses, through: :category_expenses
+  has_many :category_expense, dependent: :destroy
+  has_many :expenses, through: :category_expense
 
   validates :name, presence: true, length: { minimum: 1, maximum: 25 }
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,7 @@
 class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
-  has_many :category_expenses, dependent: :destroy
-  has_many :categories, through: :category_expenses
+  has_many :category_expense, dependent: :destroy
+  has_many :categories, through: :category_expense
 
   validates :name, presence: true, length: { minimum: 1, maximum: 25 }
   validates :amount, presence: true, numericality: { greater_than: 0 }
