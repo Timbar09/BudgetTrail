3740b029dac4ca111e2817c70f73b9b689a90ba7
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-04 20:10:00 +0200

    Add Category model

---
+++ app/models/category.rb
@@ -0,0 +1,7 @@
+class Category < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  has_many :category_expenses, dependent: :destroy
+  has_many :expenses, through: :category_expenses
+
+  validates :name, presence: true, length: { minimum: 1, maximum: 25 }
+end
