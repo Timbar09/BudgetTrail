58df5b68599722e63809054ade3757dfebdffceb
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-06 16:43:48 +0200

    Add expenses index action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,6 +9,7 @@ class CategoriesController < ApplicationController
 
   def create
     @category = current_user.categories.new(category_params)
+    
     if @category.save
       redirect_to categories_path, notice: 'Category created successfully'
     else
--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,4 +1,13 @@
 class ExpensesController < ApplicationController
+  before_action :set_category, only: [:index]
+
   def index
+    @expenses = @category.expenses
+  end
+
+  private
+
+  def set_category
+    @category = current_user.categories.find(params[:category_id])
   end
 end
