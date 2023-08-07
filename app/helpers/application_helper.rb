464c38ebe62d46c433d0b736dea64120d67786e7
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-07 18:17:32 +0200

    Refactor current_page_title helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,21 @@
 module ApplicationHelper
+  PAGE_TITLES = {
+    'categories#index' => 'Categories',
+    'categories#new' => 'Add a new category',
+    'expenses#index' => 'Transactions',
+    'expenses#new' => 'Add a new transaction',
+    'sessions#new' => 'Log In',
+    'registrations#new' => 'Sign Up',
+    'registrations#edit' => 'Edit Profile',
+    'passwords#new' => 'Forgot Password',
+    'passwords#edit' => 'Change Password',
+    'confirmations#new' => 'Resend Confirmation',
+    'unlocks#new' => 'Resend Unlock Instructions'
+  }.freeze
+
+  DEFAULT_TITLE = 'BudgetTrail'.freeze
+
   def current_page_title
-    if controller_name == 'categories' && action_name == 'index'
-      'Categories'
-    elsif controller_name == 'categories' && action_name == 'new'
-      'Add a new category'
-    elsif controller_name == 'expenses' && action_name == 'index'
-      'Transactions'
-    elsif controller_name == 'expenses' && action_name == 'new'
-      'Add a new transaction'
-    elsif controller_name == 'sessions'
-      'Log In'
-    elsif controller_name == 'registrations' && action_name == 'new'
-      'Sign Up'
-    elsif controller_name == 'registrations' && action_name == 'edit'
-      'Edit Profile'
-    elsif controller_name == 'passwords' && action_name == 'new'
-      'Forgot Password'
-    elsif controller_name == 'passwords' && action_name == 'edit'
-      'Change Password'
-    elsif controller_name == 'confirmations'
-      'Resend Confirmation'
-    elsif controller_name == 'unlocks'
-      'Resend Unlock Instructions'
-    else
-      'BudgetTrail'
-    end
+    PAGE_TITLES["#{controller_name}##{action_name}"] || DEFAULT_TITLE
   end
 end
