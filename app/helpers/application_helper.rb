9da4bc22298c539a6c1b9a142494038d03a5a654
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-07 08:56:32 +0200

    Add current page title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  def current_page_title
+    if controller_name == 'categories' && action_name == 'index'
+      'Categories'
+    elsif controller_name == 'categories' && action_name == 'new'
+      'New Category'
+    elsif controller_name == 'expenses' && action_name == 'index'
+      'Transactions'
+    elsif controller_name == 'expenses' && action_name == 'new'
+      'New Transaction'
+    elsif controller_name == 'sessions'
+      'Log In'
+    elsif controller_name == 'registrations' && action_name == 'new'
+      'Sign Up'
+    elsif controller_name == 'registrations' && action_name == 'edit'
+      'Edit Profile'
+    elsif controller_name == 'passwords' && action_name == 'new'
+      'Forgot Password'
+    elsif controller_name == 'passwords' && action_name == 'edit'
+      'Change Password'
+    elsif controller_name == 'confirmations'
+      'Resend Confirmation'
+    elsif controller_name == 'unlocks'
+      'Resend Unlock Instructions'
+    else
+      'BudgetTrail'
+    end
+  end
 end
