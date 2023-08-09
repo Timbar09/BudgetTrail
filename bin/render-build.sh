48b0236ef1cc004141641abd7704255d8662ffec
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-09 14:00:58 +0200

    Revert script changes

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,4 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:drop db:create db:migrate
+bundle exec rake db:migrate
