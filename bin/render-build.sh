9b07053c18d3581074dd318c5b64ec5646058ff7
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-09 13:42:20 +0200

    Add enviro check to script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,12 +5,4 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-
-# Drop the existing
-bundle exec rake db:drop
-
-# Create a new database
-bundle exec rake db:create
-
-# Migrate the new database
-bundle exec rake db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:drop db:create db:migrate
