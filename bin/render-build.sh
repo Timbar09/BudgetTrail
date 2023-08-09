b4b9ed891fb08a6d8b1f217cea352cb064bcdc95
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-09 13:34:32 +0200

    Update db script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,12 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+
+# Drop the existing
+bundle exec rake db:drop
+
+# Create a new database
+bundle exec rake db:create
+
+# Migrate the new database
 bundle exec rake db:migrate
